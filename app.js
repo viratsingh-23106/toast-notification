9598ce0eb22b2ed07e473413769dc4f1b6cb284d
viratsingh <singhvirat1890@gmail.com>
2024-12-15 12:24:27 +0530

    toast notification

---
+++ app.js
@@ -0,0 +1,31 @@
+let toastBox= document.getElementById("toastBox");
+let successMsg = ' <i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = ' <i class="fa-solid fa-circle-xmark"></i> Plz fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input msg, Check again';
+
+
+
+
+
+
+
+
+function showToast(msg){
+   let toast =document.createElement('div');
+   toast.classList.add('toast');
+   toast.innerHTML = msg;
+   toastBox.appendChild(toast);
+
+   if(msg.includes('error')){
+    toast.classList.add('error');
+}
+
+if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+}
+
+setTimeout(()=>{
+    toast.remove();
+},6000)
+
+}
